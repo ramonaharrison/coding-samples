183837e1f26feb584ca8095f1b15e1fa955b6158
Ramona Harrison <ramona.e.harrison@gmail.com>
2015-02-05 12:45:09 -0500

    lmc

--- little_man_computer.rb
+++ little_man_computer.rb
@@ -14,11 +14,6 @@
 
 def perform(instruction)
 
-  @calculator_display = 0
-  @mailbox = [901, 306, 901, 106, 902, 0]
-  @in_basket = []
-  @out_basket = []
-
   xx = instruction % 100
 
   if instruction < 10
@@ -30,39 +25,39 @@ def perform(instruction)
   first, second, third = instruction.split('')
 
   if first == '0'                        # STOP
-    puts(out_basket)
+    puts(@out_basket)
     if first == '1'                     # ADD xx
-      calculator_display += xx
+      @calculator_display += xx
     elsif first == '2'                     # SUB xx
-      calculator_display -= xx
+      @calculator_display -= xx
     elsif first == '3'                     # STO xx
-      mailbox[xx] = calculator_display
+      @mailbox[xx] = @calculator_display
     elsif first == '4'                     # STA xx
     elsif first == '5'                     # LOAD xx
-      calculator_display = mailbox[xx]
+      @calculator_display = @mailbox[xx]
     elsif first == '6'                     # B xx
-      instruction_counter = xx
+      @instruction_counter = xx
     elsif first == '7'                     # BZ xx
-      if calculator_display == 0
-        instruction_counter = xx
+      if @calculator_display == 0
+        @instruction_counter = xx
       end
     elsif first == '8'                     # BP
-      if calculator_display >= 0
-        instruction_counter = xx
+      if @calculator_display >= 0
+        @instruction_counter = xx
       end
     elsif first == '9' && third == '1'               # READ
-      calculator_display = in_basket.shift
+      @calculator_display = @in_basket.shift
     elsif first == '9' && third == '2'               # PRINT
-      out_basket.push(calculator_display)
+      @out_basket.push(@calculator_display)
     else
       puts "Error in instruction set" + first + second + third
     end
   end
 
-  while instruction_counter < mailbox.length
-    @current_instruction = mailbox[instruction_counter]
+  while @instruction_counter < @mailbox.length
+    @current_instruction = @mailbox[@instruction_counter]
     @instruction_counter += 1
-    perform(current_instruction)
+    perform(@current_instruction)
   end
-  puts(out_basket)
+  puts(@out_basket)
 end
